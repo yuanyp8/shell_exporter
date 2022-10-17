b47c2fa5b7dda40e889ca2b3e3960a2cb6b988d0
元玉鹏 <yuanyupeng@cmict.chinamobile.com>
2022-10-17 15:26:32 +0800

    v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
 exporter() {
-   /shell_exporter --collector.textfile.directory ${DIR} --web.listen-address ${PORT}
+   /shell_exporter --collector.textfile.directory ${DIR} --web.listen-address ${PORT} &
 }
 
 crond() {
     /usr/sbin/crond -n 
 }
 
+
+add_cron() {
+mkdir -p /var/spool/cron/
+cat >> /var/spool/cron/root <<EOF
+*/1 * * * * /cron.sh
+EOF
+chmod +x /cron.sh
+}
+
 main() {
     exporter
+    add_cron
     crond
     /bin/bash
 }
